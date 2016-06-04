8969a515b46046ab0cb328fe9953a2c0f6dd4499
Vikram Jain <vjain40@gatech.edu>
2016-06-04 00:30:05 -0400

    gitignore

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var request = require('request');
-app.get('/', function (req, res) {
+
+app.get('/hi', function (req, res) {
     request('https://parkenhance.firebaseio.com//.json?print=pretty', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body)
+            res.send(body);
         }
     })
 
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
