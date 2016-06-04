efc7538ce086820e88dc74728959ee157228ec3f
Vikram Jain <vjain40@gatech.edu>
2016-06-04 01:40:05 -0400

    near_lots works

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ var app = express();
 var http = require('http');
 var request = require('request');
 
-app.get('/hi', function (req, res) {
+// get data about nearby parking lots
+app.get('/near_lots', function (req, res) {
     request('https://parkenhance.firebaseio.com//.json?print=pretty', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body);
+
+            var lots = JSON.parse(response.body);
+            var lotStatus = {}
+            for (var lot in lots){
+                var details = {}
+                details['latitude'] = lots[lot]['latitude']
+                details['longitude'] = lots[lot]['longitude']
+                details['empty_spots'] = lots[lot]['empty_spots']
+                details['parking_spots'] = lots[lot]['parking_spots']
+                var ratio = details['empty_spots']/details['parking_spots'];
+                switch (true){
+                    case (ratio==0): details['status'] = 'full'; break;
+                    case (ratio==1): details['status'] = 'empty'; break;
+                    case (ratio<=0.5): details['status'] = 'relatively full'; break;
+                    case (ratio>0.5): details['status'] = 'relatively empty'; break;
+                }
+                lotStatus[lot] = details
+            }
+            res.send(JSON.stringify(lotStatus, null, 2));
+        } else {
+            res.status(404).send("501 Error");
         }
     })
 
