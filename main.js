4d29daaca06020778893045046c9118a3e34cc19
Vikram Jain <vjain40@gatech.edu>
2016-07-12 21:49:07 -0400

    cors

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http');
 var request = require('request');
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 var braintree = require("braintree");
 var gateway = braintree.connect({
@@ -14,26 +15,8 @@ var gateway = braintree.connect({
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-//git push heroku master
 
 
 // client token
